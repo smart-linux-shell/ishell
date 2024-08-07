f3fb75b05b12ccf828b455a7beb5d3b7ebd55f59
Iulian Alexa <116089722+iulianalexa@users.noreply.github.com>
2024-08-08 02:36:28 +0300

    Debian repository Docker image workflow
    Signed-off-by: Iulian Alexa <116089722+iulianalexa@users.noreply.github.com>

--- webserver/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t nginx-ssh .
-docker run -d --name nginx-ssh -p 80:80 -p 2222:22 -v www-data:/var/www/html nginx-ssh
--- webserver/stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker stop nginx-ssh
-docker rm nginx-ssh
