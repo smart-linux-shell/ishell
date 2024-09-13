c779817a3621ed526f569d943d59ec8e2c1a7c0b
coma007 <milicasladakovic@gmail.com>
2024-09-13 12:17:59 +0200

    feat: assistant bookmarking

--- tui-tux/assistant.cpp
+++ tui-tux/assistant.cpp
@@ -4,8 +4,15 @@
 #include <stdlib.h>
 
 #include <iostream>
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <sstream>
 
 #include "agency_request_wrapper.hpp"
+
+// <alias:<command, result>>
+std::unordered_map<std::string, std::pair<std::string, std::string>> bookmarks;
 
 void assistant() {
     using_history();
@@ -17,11 +24,40 @@ void assistant() {
             break;
         }
 
-        add_history(input);
+        std::string input_str(input);
 
-        std::string result = ask_agent("http://127.0.0.1:5000/agents/assistant", std::string(input));
+        if (input_str.find("bookmark") == 0) {
+            std::istringstream iss(input_str);
+            std::string cmd, alias;
+            int index;
+\
+            iss >> cmd >> index >> alias;
 
-        std::cout << result << "\n";
+            if (index > 0 && index <= history_length) {
+                HIST_ENTRY *he = history_get(history_length - index + 1);
+                if (he) {
+                    std::string command(he->line);
+                    // TODO save outputs
+                    std::string result = ask_agent("http://127.0.0.1:5000/agents/assistant", command);
+                    bookmarks[alias] = {command, result};
+                    std::cout << "Bookmarked command: \"" << command << "\" with alias: \"" << alias << "\"\n";
+                }
+            } else {
+                std::cout << "Invalid history index.\n";
+            }
+        }
+        // TODO change so that not every query is searched through bookmarks
+        else if (bookmarks.find(input_str) != bookmarks.end()) {
+            std::string command = bookmarks[input_str].first;
+            std::string result = bookmarks[input_str].second;
+
+            std::cout << "Executing bookmarked command \"" << command << "\" with result: " << result << "\n";
+        }
+        else {
+            add_history(input);
+            std::string result = ask_agent("http://127.0.0.1:5000/agents/assistant", input_str);
+            std::cout << result << "\n";
+        }
 
         free(input);
     }
