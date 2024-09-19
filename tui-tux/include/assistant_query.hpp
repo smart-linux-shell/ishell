4734a582e73bff855a9ba9bb4f98b7b75e041066
coma007 <milicasladakovic@gmail.com>
2024-09-19 20:36:03 +0200

    add: bookmark test

--- tui-tux/include/assistant_query.hpp
+++ tui-tux/include/assistant_query.hpp
@@ -1,7 +1,11 @@
-#include <fstream>
-#include <sstream>
-#include <iostream>
+#ifndef ASSISTANT_QUERY_HPP
+#define ASSISTANT_QUERY_HPP
+
+#include <string>
 #include <vector>
+#include <utility>
 
 void get_agency_url();
 std::string execute_query(const std::string &query, std::vector<std::pair<std::string, std::string>> &session_history);
+
+#endif // ASSISTANT_QUERY_HPP
--- tui-tux/include/bookmarks.hpp
+++ tui-tux/include/bookmarks.hpp
@@ -1,21 +1,34 @@
 #ifndef BOOKMARKS_HPP
 #define BOOKMARKS_HPP
 
-#include <string>
 #include <unordered_map>
+#include <string>
 #include <vector>
 #include <utility>
+#include "../nlohmann/json.hpp"
+using json = nlohmann::json;
 
+// <alias:<query, result>>
 extern std::unordered_map<std::string, std::pair<std::string, std::string>> bookmarks;
 
+bool create_bookmarks_file(const std::string &filename);
+void parse_bookmark_json(const nlohmann::json &bookmark);
 void load_bookmarks(const std::string &filename);
 void save_bookmarks(const std::string &filename);
 bool is_bookmark_command(const std::string &input_str);
 bool is_bookmark_flag(const std::string &option);
-bool is_bookmark(const std::string &alias);
-void remove_bookmark(const std::string &alias);
 bool is_remove_flag(const std::string &option);
+bool is_bookmark(const std::string &alias);
 std::pair<std::string, std::string> get_bookmark(const std::string &alias);
+bool is_list_flag(const std::string &input);
+bool is_help_flag(const std::string &input);
+std::string get_query_from_history(int index);
+std::string find_result_in_session_history(const std::string &query, std::vector<std::pair<std::string, std::string>> &session_history);
+void bookmark(int index, const std::string &alias, std::vector<std::pair<std::string, std::string>> &session_history);
+void remove_bookmark(const std::string &alias);
+void list_bookmarks();
+void help();
+bool try_parse_bookmark_command(const std::string &input_str, std::string &cmd, int &index, std::string &alias);
 void handle_bookmark_command(const std::string &input_str, std::vector<std::pair<std::string, std::string>> &session_history);
 
-#endif
+#endif // BOOKMARKS_HPP
