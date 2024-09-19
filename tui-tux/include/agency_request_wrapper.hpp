b0812c1b3936a2777405728856d104577e1aa50f
coma007 <milicasladakovic@gmail.com>
2024-09-19 20:36:03 +0200

    add: bookmark test

--- tui-tux/include/agency_request_wrapper.hpp
+++ tui-tux/include/agency_request_wrapper.hpp
@@ -1,8 +1,17 @@
-#ifndef AGENCY_REQUEST_WRAPPER_H
-#define AGENCY_REQUEST_WRAPPER_H
+#ifndef AGENCY_REQUEST_WRAPPER_HPP
+#define AGENCY_REQUEST_WRAPPER_HPP
 
 #include <string>
+#include <vector>
+#include "../nlohmann/json.hpp"
 
-std::string ask_agent(const std::string& url, const std::string& user_query);
+using json = nlohmann::json;
+std::string get_linux_distro();
+std::vector<std::string> get_installed_packages();
+std::string get_ssh_ip();
+int get_ssh_port();
+std::string get_ssh_user();
+json send_request_to_agent_server(const std::string &url, const std::string &user_query);
+std::string ask_agent(const std::string &url, const std::string &user_query);
 
-#endif
+#endif // AGENCY_REQUEST_WRAPPER_HPP
--- tui-tux/include/https_client.hpp
+++ tui-tux/include/https_client.hpp
@@ -1,13 +1,13 @@
-#ifndef HTTPS_CLIENT_H
-#define HTTPS_CLIENT_H
+#ifndef HTTPS_CLIENT_HPP
+#define HTTPS_CLIENT_HPP
 
 #include <string>
 #include <map>
+#include <curl/curl.h>
 #include "../nlohmann/json.hpp"
 
 using json = nlohmann::json;
 
-// Enum class to define HTTP request types
 enum class HttpRequestType {
     GET,
     POST,
@@ -18,18 +18,16 @@ enum class HttpRequestType {
     PATCH
 };
 
-// Function to make an HTTP request
-// Parameters:
-// - request_type: Type of the HTTP request (GET, POST, PUT, DELETE, HEAD, OPTIONS, PATCH)
-// - url: The URL for the request
-// - query_params: Optional map of query parameters (default is empty)
-// - body: Optional JSON body for POST, PUT, PATCH requests (default is nullptr)
-// - headers: Optional map of headers (default is empty)
-// Returns: JSON object with the response data
-json make_http_request(HttpRequestType request_type,
-                       const std::string& url,
+json make_http_request(HttpRequestType request_type, const std::string& url,
                        const std::map<std::string, std::string>& query_params = {},
                        const json& body = nullptr,
                        const std::map<std::string, std::string>& headers = {});
 
-#endif // HTTPS_CLIENT_H
+std::string build_query_string(const std::map<std::string, std::string>& query_params);
+void set_request_type(CURL* curl, HttpRequestType request_type);
+void add_request_body(CURL* curl, HttpRequestType request_type, const json& body, std::string& jsonData);
+void add_request_headers(CURL* curl, const std::map<std::string, std::string>& headers);
+void set_response_callbacks(CURL* curl, std::string& readBuffer, std::map<std::string, std::string>& response_headers);
+json perform_request(CURL* curl);
+
+#endif // HTTPS_CLIENT_HPP
