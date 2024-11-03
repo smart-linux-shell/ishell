cde351d842704d59896ab7b4a4a5fee467a4a57b
Kirill Krinkin (box) <kirill@krinkin.com>
2024-11-03 14:22:19 +0200

    testing

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ def serve_deb_repo(filename):
         if os.path.exists(filepath):
             log.info(f"Serving file {filepath}.")
             # Get the directory path and actual filename
-            directory = os.path.dirname(os.path.join('/data', filename))
+            directory = os.path.dirname(os.path.join('/deb', filename))
             basename = os.path.basename(filename)
             return send_from_directory(directory, basename)
         else:
